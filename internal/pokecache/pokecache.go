1d13ef1a3587ceee2165e3f2e0969367f94d2d27
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-07-07 18:19:57 +0100

    Implement caching with tests

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,60 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mu    *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	go c.reaploop(interval)
+
+	return c
+}
+
+func (c *Cache) Add(key string, value []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       value,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	value, ok := c.cache[key]
+	return value.val, ok
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add((-last))) {
+			delete(c.cache, k)
+		}
+	}
+}
+
+func (c *Cache) reaploop(interval time.Duration) {
+	ticker := time.NewTicker((interval))
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
